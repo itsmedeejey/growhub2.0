6319ffa508039b2e9ba9609faf1cc07f88ae9036
Dhanjyoti <dhanjyotidas00@gmail.com>
2025-06-14 12:26:41 +0530

    added category banner for mobile screen and desktop separate

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -2,18 +2,20 @@ import  Navbar  from "@/components/Navbar"
 import  Footer  from "@/components/Footer"
 import  Categories  from "@/testData/catagoryData"
 import  CategoryCard  from "@/components/categoryCard"
+import Banner from "@/components/banner"
+import Image from "next/image"
 export default function CatagoriesPage(){
-
     return(
         <div>
-            <div className="m-8">
+            <Image className="hidden md:block"  src={"/banner/category.png"} width={1920} height={400}   alt={"banner"}></Image>
+            <Image className=" md:hidden"  src={"/banner/category-mobile.png"} width={1920} height={400} alt={"banner"}></Image>
+
+            <div className="mx-8">
             <div className="flex justify-center ">
-                <h1 className="text-amber-900  text-2xl md:text-4xl font- font-">Categories</h1>
+                <h1 className="text-amber-900  text-xl md:text-3xl mt-4  md:mt-8 font-thin">Categories</h1>
             </div >    
              <CategoryCard categories={Categories}></CategoryCard>
             </div>
-
-
 
         </div>
     )
